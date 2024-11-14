1f71c613c0a1a7741d23ac1718e9f8d3c51fa937
Seggan <seggan21@gmail.com>
2024-11-14 11:12:59 -0500

    Initial commit idk what im doing aaaaa

---
+++ src/client/kotlin/io/github/seggan/deathview/client/DeathViewClient.kt
@@ -0,0 +1,9 @@
+package io.github.seggan.deathview.client
+
+import net.fabricmc.api.ClientModInitializer
+
+class DeathViewClient : ClientModInitializer {
+
+    override fun onInitializeClient() {
+    }
+}
---
+++ src/main/kotlin/io/github/seggan/deathview/DeathView.kt
@@ -0,0 +1,9 @@
+package io.github.seggan.deathview
+
+import net.fabricmc.api.ModInitializer
+
+class DeathView : ModInitializer {
+
+    override fun onInitialize() {
+    }
+}
