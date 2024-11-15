9c88fc8fc22aaca2e0a26c2a8b413669abc3d539
Seggan <seggan21@gmail.com>
2024-11-15 12:56:14 -0500

    Switch to front view if too close

--- src/client/kotlin/io/github/seggan/deathview/client/PlayerDeathHandler.kt
+++ src/client/kotlin/io/github/seggan/deathview/client/PlayerDeathHandler.kt
@@ -4,6 +4,8 @@ package io.github.seggan.deathview.client
 
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.option.Perspective
+import net.minecraft.util.hit.HitResult
+import net.minecraft.world.RaycastContext
 
 var recentDeath = false
 private var lastPerspective = Perspective.FIRST_PERSON
@@ -11,14 +13,37 @@ var originalChatOpacity = 0.0
 
 fun onDeath() {
     recentDeath = true
-    val options = MinecraftClient.getInstance().options
-    lastPerspective = options.perspective
-    options.perspective = Perspective.THIRD_PERSON_BACK
+    val client = MinecraftClient.getInstance()
+    val options = client.options
     originalChatOpacity = options.textBackgroundOpacity.value
+
+    lastPerspective = options.perspective
+    val player = client.player
+    if (player == null) {
+        options.perspective = Perspective.THIRD_PERSON_BACK
+    } else {
+        val headPos = player.getCameraPosVec(1f)
+        val cameraDir = player.getRotationVec(1f)
+        val twoBlocksBack = headPos.subtract(cameraDir.multiply(2.0))
+        val raycastContext = RaycastContext(
+            headPos,
+            twoBlocksBack,
+            RaycastContext.ShapeType.OUTLINE,
+            RaycastContext.FluidHandling.NONE,
+            player
+        )
+        val hit = player.world.raycast(raycastContext)
+        if (hit != null && hit.type == HitResult.Type.MISS) {
+            options.perspective = Perspective.THIRD_PERSON_BACK
+        } else {
+            options.perspective = Perspective.THIRD_PERSON_FRONT
+        }
+    }
 }
 
 fun onRespawn() {
     val options = MinecraftClient.getInstance().options
     options.perspective = lastPerspective
     options.textBackgroundOpacity.value = originalChatOpacity
+    recentDeath = false
 }
