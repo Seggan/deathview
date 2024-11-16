31d69cf8795c418285beaa133cebf455f35106f2
Seggan <seggan21@gmail.com>
2024-11-15 20:09:23 -0500

    Inject head

--- src/client/java/io/github/seggan/deathview/client/mixin/ServerPlayerMixin.java
+++ src/client/java/io/github/seggan/deathview/client/mixin/ServerPlayerMixin.java
@@ -10,7 +10,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(ServerPlayerEntity.class)
 public abstract class ServerPlayerMixin {
 
-    @Inject(method = "onDeath", at = @At("TAIL"))
+    @Inject(method = "onDeath", at = @At("HEAD"))
     private void onDeath(CallbackInfo ci) {
         PlayerDeathHandler.onDeath();
     }
